516e891f21d6c057ceef63e7aa921dc97a316fa1
Kai Hao <kevin830726@gmail.com>
2021-01-27 16:35:17 +0800

    Fix Truncate exports

--- packages/components/src/truncate/index.js
+++ packages/components/src/truncate/index.js
@@ -1,3 +1,7 @@
-export { default as Truncate } from './truncate';
+/**
+ * Internal dependencies
+ */
+import Truncate from './truncate';
 
 export * from './use-truncate';
+export default Truncate;
